9ec92b36ecd048d59ca61829654290c6cb8dce5f
VietDucNg <vietducnguyen.fit@gmail.com>
2026-04-20 09:54:08 +0200

    build: add code to rm default css in init_react.sh

--- init_react.sh
+++ init_react.sh
@@ -1,7 +1,12 @@
 npm create vite@latest . -- --template react
 
+rm src/index.css
+rm src/App.css
+
 mkdir src/styles
 mkdir src/components
 
 mv reset.css src/styles
 mv style.css src/style
+
+mv src/App.jsx src/components/App.jsx
