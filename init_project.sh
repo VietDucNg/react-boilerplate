6f836edb13a78f058aefbf71567563f2e3368951
VietDucNg <vietducnguyen.fit@gmail.com>
2026-06-23 14:51:39 +0200

    build: add code to rm unused default img

--- init_project.sh
+++ init_project.sh
@@ -9,3 +9,8 @@ mv App.jsx src/App.jsx
 npm install @mui/material @emotion/react @emotion/styled
 npm install @fontsource/roboto
 npm install @mui/icons-material
+
+sed -i "/import '\.\/index\.css'/d" main.jsx
+
+rm public/icons.svg
+rm src/assets/*
